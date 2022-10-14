a1bf8109bc483740cff8176408242adfb7aaa9f6
Chathra Navoda <chathra96@gmail.com>
2022-10-14 12:30:24 +0530

    user login and registration

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,50 @@
+import React from "react";
+
 const HomeView = () => {
   return (
-    <div style={{ marginTop: "175px" }}>
-      {/* It has four buttons to navigate to another functions */}
-      <div class="container">
-        <button class="btn btn1">Donations</button>
-        <button class="btn btn2">Doonor</button>
-        <button class="btn btn3">Beneficiaries</button>
-        <button class="btn btn4">Volunteers</button>
+    <div class="row" style={{ marginTop: "250px" }}>
+      <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+        <div class="row">
+          <div class="col-lg-3 col-md-3 col-3 col-sm-3"></div>
+          <div class="col-lg-6 col-md-6 col-6 col-sm-6">
+            <div class="row" style={{ marginBottom: "20px" }}>
+              <div class="col-lg-6 col-md-6 col-6 col-sm-6">
+                <a href="/all_donors">
+                  <div class="card" style={{ height: "5rem" }}>
+                    <div class="card-body">
+                      <p class="card-text">Donor Management</p>
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-6 col-md-6 col-6 col-sm-6">
+                <div class="card" style={{ height: "5rem" }}>
+                  <div class="card-body">
+                    <p class="card-text">Volunteer Management</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-lg-6 col-md-6 col-6 col-sm-6">
+                <div class="card">
+                  <div class="card-body" style={{ height: "5rem" }}>
+                    <p class="card-text">Donations Management</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 col-6 col-sm-6">
+                <div class="card">
+                  <div class="card-body" style={{ height: "5rem" }}>
+                    <p class="card-text">Beneficiary Management</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-3 col-sm-3"></div>
+        </div>
       </div>
     </div>
   );
