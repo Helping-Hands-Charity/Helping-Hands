99caa54b106588b88a86e784604e534cedaecff5
Chathra Navoda <chathra96@gmail.com>
2022-10-20 13:22:51 +0530

    backend completed

--- server/models/donor.js
+++ server/models/donor.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const donorSchema = mongoose.Schema({
-<<<<<<< HEAD
   firstName: String,
   lastName: String,
   email: String,
@@ -18,21 +17,3 @@ const donorSchema = mongoose.Schema({
 
 const DonorModel = mongoose.model("Donor", donorSchema);
 export default DonorModel;
-=======
-    firstName: String,
-    lastName: String,
-    email: String,
-    nic: String,
-    address: String,
-    phone: String,
-    createdAt:{
-        type: Date,
-        default: new Date(),
-    },
-    creator: String,
-    name: String
-});
-
-const DonorModel = mongoose.model("Donor", donorSchema);
-export default DonorModel;
->>>>>>> de0bd18b104782e742ae7fc270b8b2616c5fdc67
