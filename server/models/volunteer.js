c5418d3afd52685db9b9fa91c7fe619b9295a8ee
Vibhu Jayawardane <vibhujay99@gmail.com>
2022-10-21 23:48:39 +0530

    Refactor() : Backend form validation

--- server/models/volunteer.js
+++ server/models/volunteer.js
@@ -4,9 +4,38 @@ const volunteerSchema = mongoose.Schema({
   title: String,
   firstName: String,
   lastName: String,
-  email: String,
-  nic: String,
-  phone: String,
+  email: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+      message: "Please enter a valid email",
+    },
+    required: [true, "Email required"],
+  },
+  nic: {
+    type: String,
+    validate: {
+      validator: function (a) {
+        return /^([0-9]{9}[x|X|v|V]|[0-9]{12})$/.test(a);
+      },
+      message: "Please enter a valid ID",
+    },
+    required: [true, "ID required"],
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: function (b) {
+        return /^(?:0|94|\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\d)\d{6}$/.test(
+          b
+        );
+      },
+      message: "Please enter a valid Phone number",
+    },
+    required: [true, "Phone required"],
+  },
   description: String,
   createdAt: {
     type: Date,
